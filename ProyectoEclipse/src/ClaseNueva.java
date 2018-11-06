bf8bc81f9cdcaca5e9311398ebe50404d1bc7f95
ChimoB <44805127+ChimoB@users.noreply.github.com>
2018-11-06 20:06:21 +0100

    Update ClaseNueva.java

--- ProyectoEclipse/src/ClaseNueva.java
+++ ProyectoEclipse/src/ClaseNueva.java
@@ -4,6 +4,10 @@ public class ClaseNueva {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Profesor");
+		System.out.println("Adios Profesor");
+		// TODO Auto-generated method stub
+		System.out.println("Hola Profesor");
+		System.out.println("Adios Profesor");
 	}
 
 }
